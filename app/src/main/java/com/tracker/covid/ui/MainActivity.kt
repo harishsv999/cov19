034031ce9d419ff6cb8ce1ba2a4b6a41456e9f3a
Sv Harish <harishsv999@gmail.com>
2020-04-25 15:59:09 +0530

    improvment in navigation.

--- app/src/main/java/com/tracker/covid/ui/MainActivity.kt
+++ app/src/main/java/com/tracker/covid/ui/MainActivity.kt
@@ -1,43 +1,50 @@
 package com.tracker.covid.ui
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.findNavController
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.tracker.covid.CoVidApplication
 import com.tracker.covid.R
-import com.tracker.covid.data.remote.repositories.CoVidRepository
-import com.tracker.covid.di.component.DaggerActivityComponent
-import com.tracker.covid.di.modules.ActivityModule
-import com.tracker.covid.ui.countries.CountriesFragment
-import com.tracker.covid.utils.NetworkHelper
-import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
+import com.tracker.covid.utils.setupWithNavController
 
 
 class MainActivity : AppCompatActivity() {
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupNavigation()
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
     }
 
-    private fun setupNavigation() {
-        val navController = findNavController(R.id.navGraph)
-        bottomBar.setupWithNavController(navController)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomBar)
+
+        val navGraphIds = listOf(
+            R.navigation.home_nav,
+            R.navigation.countries_nav,
+            R.navigation.live_nav,
+            R.navigation.about_nav
+        )
+
+        bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.navGraph
+        )
     }
 
     override fun onSupportNavigateUp() =
--- app/src/main/java/com/tracker/covid/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/tracker/covid/ui/home/HomeAdapter.kt
@@ -1,21 +1,22 @@
 package com.tracker.covid.ui.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.tracker.covid.R
 import com.tracker.covid.data.remote.model.Country
-import com.tracker.covid.data.remote.response.GlobalSummary
 import kotlinx.android.synthetic.main.home_item.view.*
 
 class HomeAdapter(private val allItems: List<Country>) :
     RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
         val inflater =
             LayoutInflater.from(parent.context).inflate(R.layout.home_item, parent, false)
-        return HomeViewHolder(inflater)
+        return HomeViewHolder(inflater, parent.context)
     }
 
     override fun getItemCount(): Int = allItems.size
@@ -24,10 +25,27 @@ class HomeAdapter(private val allItems: List<Country>) :
         holder.bind(allItems[position])
     }
 
-    class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val hCountryName: TextView = view.countryName
+    class HomeViewHolder(view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
+        private val countryName = view.countryName
+        private val countryCode = view.flag
+        private val newInfected = view.newInfected
+        private val totalInfected = view.totalInfected
+        private val newRecovery = view.newRecover
+        private val totalRecovery = view.totalRecover
+        private val newDeaths = view.newDeaths
+        private val totalDeath = view.totalDeaths
+
         fun bind(items: Country) {
-            hCountryName.text = items.countryName
+            countryName.text = items.countryName
+
+            newInfected.text = items.newConfirmedCases.toString()
+            totalInfected.text = items.totalConfirmedCases.toString()
+
+            newRecovery.text = items.newRecoveredCases.toString()
+            totalRecovery.text = items.totalRecoveredCases.toString()
+
+            newDeaths.text = items.newDeathCases.toString()
+            totalDeath.text = items.totalDeathCases.toString()
         }
     }
 
